8f4dec7e49700a5c5dc0d6d3c6fbe2c10a9a1f3c
Alok <alok.alps89@gmail.com>
2014-02-23 16:47:39 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foodwin::Application.config.secret_key_base = '2fffc1dc0ab46a1764099592fe51c84baa23efc9b67fd161249f4135a058bab60cc38bac7b4886de0875f8db3d405103cb1effb7d03efcdc15df77e380f0aa23'
